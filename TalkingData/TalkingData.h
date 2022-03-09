5293773d144ac15695d01ca4d68c6de01b6bb7d7
Elf Sundae <elf.sundae@gmail.com>
2022-03-09 22:56:27 +0800

    Add TalkingData files

---
+++ TalkingData/TalkingData.h
@@ -0,0 +1,10 @@
+//
+//  TalkingData.h
+//  TalkingData
+//
+//  Created by Elf Sundae on 2022/03/09.
+//  Copyright © 2022 https://0x123.com. All rights reserved.
+//
+
+#import "TalkingDataSDK.h"
+#import "UIDevice+TalkingDataExtension.h"
---
+++ TalkingData/UIDevice+TalkingDataExtension.m
@@ -0,0 +1,19 @@
+//
+//  UIDevice+TalkingDataExtension.m
+//  TalkingData
+//
+//  Created by Elf Sundae on 2022/03/09.
+//  Copyright © 2022 https://0x123.com. All rights reserved.
+//
+
+#import "UIDevice+TalkingDataExtension.h"
+#import "TalkingDataSDK.h"
+
+@implementation UIDevice (TalkingDataExtension)
+
+- (NSString *)talkingDataDeviceIdentifier
+{
+    return [TalkingDataSDK getDeviceID];
+}
+
+@end
